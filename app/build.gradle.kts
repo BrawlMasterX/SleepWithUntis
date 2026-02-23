5e0f806114b758e2b21529d3f618fd2ef2a1daf1
BrawlMasterX <noahkolb9@gmail.com>
2026-02-23 16:54:59 +0100

    versionName higher

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 25
         targetSdk = 33
         versionCode = 1
-        versionName = "2.7"
+        versionName = "2.8"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             abiFilters += listOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
